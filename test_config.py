f8a92407bbbbb877d48647f83c6b87905bfd0ea9
Flavio Barboza <flmbarboza@gmail.com>
2026-08-04 11:41:57 -0300

    Delete test_config.py

--- test_config.py
+++
@@ -1,9 +0,0 @@
-from config import settings, theme, app_logger
-from config.paths import create_directories
-
-create_directories()
-
-print(settings.APP_NAME)
-print(settings.VERSION)
-print(theme.PRIMARY)
-app_logger.info("SIPE iniciado com sucesso.")
